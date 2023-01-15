421f3d99beaf63410a2fd53c2721abcd7e9d88ef
Iwona <iwona.ogrodowska@epoczta.pl>
2023-01-15 17:51:51 +0100

    Dodaj pliki projektów.

---
+++ CardGame/MainBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CardGame
+{ 
+    public class MainBoard : View
+    {
+       
+        public override string imagePath => $"pack://application:,,,/images/boardproperty/fieldback.png";
+       
+    }
+}
